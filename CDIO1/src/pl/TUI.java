41763ec9fdc85d86bb6196411b09ab8e934b23c5
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-02-14 11:58:29 +0100

    Made basic menu. Bugs present
    The menu will keep looping if the user types in anything else than an
    integer.

--- CDIO1/src/pl/TUI.java
+++ CDIO1/src/pl/TUI.java
@@ -7,15 +7,64 @@ import dto.UserDTO;
 public class TUI {
 	
 	public static void main(String [] args){
-		
+		TUI textInterface = new TUI();
+		textInterface.menu();
 	}
 	
 	public TUI(){
-		
 	}
 	
 	public void menu(){
+		Scanner input = new Scanner(System.in); //Make new scanner for user inputs
 		
+		int choice = 0;
+		boolean looping = true;
+		
+		while(looping){
+			
+			//System message before user input
+			System.out.println("Select an option by typing the corresponding number and pressing \"enter\"");
+			System.out.println("1. Create a new user");
+			System.out.println("2. Show all existing users");
+			System.out.println("3. Update an existing user");
+			System.out.println("4. Delete an existing user");
+			System.out.println("5. Close the program");
+			
+			//Check if user input is an int
+			if(input.hasNextInt()){
+				choice = input.nextInt(); //Assign this value as a choice
+			}
+			/*
+			 * Determine which method to run. 
+			 * If the choice does not correspond to a case, 
+			 * the user is given the option to type a new number
+			 */
+			switch(choice){
+			case 1: looping = false;
+					input.close(); //Close the input
+					createUser();  
+					break;
+			case 2: looping = false;
+					input.close(); //Close the input
+					showUsers();
+					break;
+			case 3: looping = false;
+					input.close(); //Close the input
+					updateUser();
+					break;
+			case 4: looping = false;
+					input.close(); //Close the input
+					deleteUser();
+					break;
+			case 5: looping = false;
+					input.close(); //Close the input
+					endProgram();
+					break;
+			default: looping = true;
+					System.out.println("You entered " + choice + " which is not a valid choice.\n");
+					break;
+			}
+		}
 	}
 	
 	public void createUser(){
