a97270c58716d746e0a47e867582503527c85dc2
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-02-14 11:26:12 +0100

    Added TUI and MiddleMan skeletons

---
+++ CDIO1/src/fl/MiddleMan.java
@@ -0,0 +1,25 @@
+package fl;
+
+import java.util.List;
+
+import dto.UserDTO;
+
+public class MiddleMan {
+	
+	public List<UserDTO> getUserList(){
+		return null;
+	}
+	
+	public void createUser(UserDTO user){
+		
+	}
+	
+	public void updateUser(UserDTO user){
+		
+	}
+	
+	public void deleteUser(int userId){
+		
+	}
+}
+
---
+++ CDIO1/src/pl/TUI.java
@@ -0,0 +1,37 @@
+package pl;
+
+public class TUI {
+	
+	public static void main(String [] args){
+		
+	}
+	
+	public TUI(){
+		
+	}
+	
+	public void menu(){
+		
+	}
+	
+	public void createUser(){
+		
+	}
+	
+	public void showUsers(){
+		
+	}
+	
+	public void updateUser(){
+		
+	}
+	
+	public void deleteUser(){
+		
+	}
+	
+	public void endProgram(){
+		
+	}
+
+}
