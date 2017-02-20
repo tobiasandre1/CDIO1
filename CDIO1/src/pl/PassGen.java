f9a83bac855abcdfa2da0de5f5a0f384e90b2526
Gustav Petersen <Rangewaver@gmail.com>
2017-02-20 16:53:57 +0100

    Fixed whitespaces i think

--- CDIO1/src/pl/PassGen.java
+++ CDIO1/src/pl/PassGen.java
@@ -14,60 +14,76 @@ public class PassGen {
 				
 		//Strings where numbers and letters gets "taken" from
 		String Alphabet = "abcdefghijklmnopqrstuvwxyz";
-		String Numbers = "1234567890";
+		final String Numbers = "1234567890";
 
 		//UpperCaseCounter, LowerCaseCounter and NumberCounter. 
 		//To ensure 3 upper/lower -case letters and 4 numbers
 		int UCC = 0, LCC = 0, NC = 0;
+		Boolean GhostLetter = true;
 		
-		for (int i = 0; i < 10; i++){
+		//Final result
+		String PasswordFull = "";
 		
-			//Decides if next char is going to be a number or letter (NextType), and if its a letter 
-			//- if its going to be upper/lower -case
-			Boolean NextUpper = gen.nextBoolean();
-			Boolean NextType = gen.nextBoolean();
+		//If there is a whitespace in final result it will try again
+		while (GhostLetter == true){
 			
-			//NextType = True = Letter
-			if (NextType == true){
+			for (int i = 0; i < 10; i++){
 				
-				//NextUpper = True = Uppercase
-				if ((NextUpper == true && UCC < 3) || (NextUpper == false && LCC == 3 && UCC < 3)){
+				//Decides if next char is going to be a number or letter (NextType), and if its a letter 
+				//- if its going to be upper/lower -case
+				Boolean NextUpper = gen.nextBoolean();
+				Boolean NextType = gen.nextBoolean();
+				
+				//NextType = True = Letter
+				if (NextType == true){
 					
-					//Choses a random char in alphabet and puts it into array
-					//Makes sure the char is gonna be uppercase
-					Alphabet = Alphabet.toUpperCase();
-					Pass[i] = Alphabet.charAt(gen.nextInt(Alphabet.length()-1));
-					UCC++;
+					//NextUpper = True = Uppercase
+					if ((NextUpper == true && UCC < 3) || (NextUpper == false && LCC == 3 && UCC < 3)){
+						
+						//Choses a random char in alphabet and puts it into array
+						//Makes sure the char is gonna be uppercase
+						Alphabet = Alphabet.toUpperCase();
+						Pass[i] = Alphabet.charAt(gen.nextInt(Alphabet.length()-1));
+						UCC++;
+						
+					} 
+					else if (LCC < 3) {
+						
+						//Same as uppercase but ensures lowercase
+						Alphabet = Alphabet.toLowerCase();
+						Pass[i] = Alphabet.charAt(gen.nextInt(Alphabet.length()-1));
+						LCC++;
+						
+					}
 					
-				} 
-				else if (LCC < 3) {
+				}
+				else if (NC < 4) {
 					
-					//Same as uppercase but ensures lowercase
-					Alphabet = Alphabet.toLowerCase();
-					Pass[i] = Alphabet.charAt(gen.nextInt(Alphabet.length()-1));
-					LCC++;
+					//If the next char isnt gonna be a letter its gonna be a number
+					Pass[i] = Numbers.charAt(gen.nextInt(Numbers.length()-1));
+					NC++;
 					
 				}
 				
 			}
-			else if (NC < 4) {
+			
+			//Puts the password together as a string
+			for (int j = 0; j < 10; j++){
 				
-				//If the next char isnt gonna be a letter its gonna be a number
-				Pass[i] = Numbers.charAt(gen.nextInt(Numbers.length()-1));
-				NC++;
+				PasswordFull = PasswordFull + Pass[j];
+				GhostLetter = false;
+				
+				//If there is a whitespace the loop will keep on going
+				if (Character.isWhitespace(Pass[j]) == true){
+					
+					GhostLetter = true;
+					
+				}
 				
 			}
-			
+	
 		}
-		
-		//Puts the password together as a string
-		String PasswordFull = "";
-		for (int j = 0; j < 10; j++){
-			
-			PasswordFull = PasswordFull + Pass[j];
-			
-		}
-		
+				
 		return PasswordFull;
 		
 	}
